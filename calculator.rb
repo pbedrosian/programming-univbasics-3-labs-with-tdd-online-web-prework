9051262cb79a1e8bb06e114c25be2474132ef157
Peter Bedrosian <peter_bedrosian@yahoo.com>
2019-12-31 18:17:41 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,8 @@
 # Add your variables here
-first_number = 22
-second_number = 11
+first_number = 11
+second_number = 22
 
 sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number 
